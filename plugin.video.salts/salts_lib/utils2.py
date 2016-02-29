01771b583a1f4fae0c09c5e49d22b98378fd025f
tknorris <tknorris@tknorris-laptop>
2016-02-29 12:50:45 -0500

    Add 2.0.11 of SALTS to Repo

--- plugin.video.salts/salts_lib/utils2.py
+++ plugin.video.salts/salts_lib/utils2.py
@@ -783,5 +783,5 @@ def _byteify(data, ignore_dicts=False):
     if isinstance(data, list):
         return [_byteify(item, ignore_dicts=True) for item in data]
     if isinstance(data, dict) and not ignore_dicts:
-        return {_byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True) for key, value in data.iteritems()}
+        return dict([(_byteify(key, ignore_dicts=True), _byteify(value, ignore_dicts=True)) for key, value in data.iteritems()])
     return data
